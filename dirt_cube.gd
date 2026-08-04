7b1d54b986d04f375b3ebf9456e125ac5de6125e
Biam22 <liamsjhaigh@gmail.com>
2026-08-04 18:54:48 +1200

    just added return so I know that it would definitely return

--- dirt_cube.gd
+++ dirt_cube.gd
@@ -19,10 +19,11 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 		$Dirt.frame = 1
 		await get_tree().create_timer(0.1).timeout
 		has_grown = true
-		
+		return
 	elif has_grown:
 		print("Plant!")
 		$Dirt.frame = 2
+		return 
 	
 		
 		
