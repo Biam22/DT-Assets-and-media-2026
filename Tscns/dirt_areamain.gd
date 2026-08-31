10e5b2d3978e7cd2e482417937502b46a9da8943
Biam22 <liamsjhaigh@gmail.com>
2026-08-31 19:44:16 +1200

    MVP DONE!

--- Tscns/dirt_areamain.gd
+++ Tscns/dirt_areamain.gd
@@ -1,27 +1,23 @@
 extends Area2D
 @onready var growth_bar: TextureProgressBar = $UI_Bar/UI_growth
-@onready var water_bar:TextureProgressBar = $UI_water/TextureProgressBar
-
+@onready var water_bar: TextureProgressBar = $UI_water/TextureProgressBar
 var has_grown: bool = false
 var Soil_ready: bool = false
 var is_growing: bool = false 
 var is_planted: bool = false 
 var is_watered: bool = false
 var is_ready_to_harvest: bool = false
-
 var water_amount: float = 0.0        
 var water_needed: float = 3.0 
 
-# Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	growth_bar.visible = false 
 	water_bar.visible = false
 	
-
 func _pop_tween() -> void:
 	var tween = create_tween()
-	tween.tween_property($DirtMain , "scale", Vector2(1.2, 1.2), 0.1)
-	tween.tween_property($DirtMain , "scale", Vector2(1, 1), 0.1)
+	tween.tween_property($DirtMain, "scale", Vector2(1.2, 1.2), 0.1)
+	tween.tween_property($DirtMain, "scale", Vector2(1, 1), 0.1)
 	await tween.finished
 	
 func _water_flash() -> void:
@@ -31,7 +27,6 @@ func _water_flash() -> void:
 	await tween.finished
 
 func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
-	
 	if not Input.is_action_just_pressed("Left_Click"):
 		return 
 		
@@ -69,7 +64,6 @@ func _on_input_event(viewport: Node, event: InputEvent, shape_idx: int) -> void:
 				Growth_timer()
 			return
 		
-
 	elif is_ready_to_harvest:
 		_harvest()
 		return
@@ -80,22 +74,19 @@ func _fill_water_bar_step() -> void:
 	tween.tween_method(_update_water_bar, water_bar.value / water_bar.max_value, target, 0.2)
 	await tween.finished
 
-
 func _update_water_bar(progress: float) -> void:
 	water_bar.value = progress * water_bar.max_value
-
 	
 func Growth_timer() -> void:
 	await Growth_bar(5.0)
 	print("Growing stage1")
 	$DirtMain.frame = 3
-	await  Growth_bar(8.0)
+	await Growth_bar(8.0)
 	print("Growing stage2")
 	$DirtMain.frame = 4
 	$Tickmain.visible = true 
 	is_ready_to_harvest = true
 	return
-	
 	
 func _harvest() -> void:
 	if not is_ready_to_harvest:
@@ -119,7 +110,13 @@ func _harvest() -> void:
 	
 	$DirtMain.scale = Vector2(1, 1)
 	$DirtMain.modulate = Color(1, 1, 1, 1)
-	$UI_Bar.visible = false
+	
+	growth_bar.visible = false
+	growth_bar.value = 0
+	water_bar.visible = false
+	water_bar.value = 0
+	
+	$Tickmain.visible = false
 	return
 	
 func Growth_bar(duration: float) -> void: 
@@ -132,12 +129,5 @@ func Growth_bar(duration: float) -> void:
 	
 	growth_bar.visible = false 
 	
-	
 func _update_bar(progress: float) -> void:
 	growth_bar.value = progress * growth_bar.max_value
-	
-	
-	
-		
-		
-		
